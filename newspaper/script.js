999ffa225951ee5e5eacdaa314ea36880f1dee5e
Yunik Maharjan <yunik.maharjan@icloud.com>
2019-04-07 13:40:00 +0545

    adapted to ekantipurs new news url

--- newspaper/script.js
+++ newspaper/script.js
@@ -1,10 +1,9 @@
+// var today = new Date();
+// var year = today.getFullYear();
+// var month = today.getMonth() + 1;
+// month = month < 10 ? "0" + month : month;
+// var date = today.getDate() < 10 ? "0" + today.getDate() : today.getDate();
 
-var today = new Date();
-var year = today.getFullYear();
-var month = today.getMonth() + 1;
-month = month < 10 ? '0' + month : month;
-var date = today.getDate() < 10 ? '0' + today.getDate() : today.getDate();
-
-var url = 'http://epaper.ekantipur.com/epaper/the-kathmandu-post/' + year + '-' + month + '-' + date + '/' + year + '-' + month + '-' + date + '.pdf';
+var url = "https://epaper.ekantipur.com/kathmandupost";
 
 window.location.href = url;
